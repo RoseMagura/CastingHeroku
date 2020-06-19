51b432d3e70df3b3f4d3f7deade876de1ced3e6c
unknown <magurarm@guilford.edu>
2020-06-18 23:09:26 -0400

    Editing setup.sh more

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-DATABASE_URL=postgres://rnepqcnlixmwvi:2519bee575c528c54829da9d1a171580b5ead791b3fd4b7e7507608c3e8c7354@ec2-52-72-65-76.compute-1.amazonaws.com:5432/dc4mc4boljurn4
+export DATABASE_URL=postgres://rnepqcnlixmwvi:2519bee575c528c54829da9d1a171580b5ead791b3fd4b7e7507608c3e8c7354@ec2-52-72-65-76.compute-1.amazonaws.com:5432/dc4mc4boljurn4
