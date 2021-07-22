b35eeaf908ca78e56bfcf84643379fd57b47bb11
RoseMagura <magurarm@guilford.edu>
2021-07-22 16:55:52 -0400

    Feat: All movie routes working (no auth)

--- routes/movies.py
+++ routes/movies.py
@@ -1,12 +1,10 @@
-import os
 from flask import Blueprint, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from CastingHeroku.database.models import Movie
+from CastingHeroku.database.models import Movie, Actor
 from CastingHeroku.auth import *
 from CastingHeroku.utilities import paginate_items
 
 bp = Blueprint('movies', __name__)
+
 
 @bp.route('/movies')
 # @requires_auth('get:movies')
@@ -23,91 +21,93 @@ def get_movies(token=''):
         'total_movies': len(selection),
     })
 
-# @app.route('/movies', methods=['POST'])
+
+@bp.route('/movies', methods=['POST'])
 # @requires_auth('post:movies')
-# def create_movie(token):
-#     body = request.get_json()
-#     new_name = body.get('name', None)
-#     new_release = body.get('release_date', None)
-#     new_actors = body.get('actors', None)
-#     actor_list = []
+def create_movie():
+    body = request.get_json()
+    print(body)
+    new_name = body.get('name', None)
+    new_release = body.get('release_date', None)
+    new_actors = body.get('actors', None)
+    actor_list = []
 
-#     for actor in new_actors:
-#         new = Actor.query.filter(Actor.name == actor).first()
-#         if new is None:
-#             abort(404)
-#         actor_list.append(new)
+    for actor in new_actors:
+        new = Actor.query.filter(Actor.name == actor).first()
+        if new is None:
+            abort(404)
+        actor_list.append(new)
 
-#     try:
-#         entry = Movie(name=new_name, release_date=new_release,
-#                         actors=actor_list)
-#         entry.insert()
+    try:
+        entry = Movie(name=new_name, release_date=new_release,
+                      actors=actor_list)
+        entry.insert()
 
-#         return jsonify({
-#             'success': True,
-#             'created': entry.id,
-#             'total_actors': len(Movie.query.all())
-#         })
+        return jsonify({
+            'success': True,
+            'created': entry.id,
+            'total_actors': len(Movie.query.all())
+        })
 
-#     except Exception as ex:
-#         print(ex)
-#         abort(422)
+    except Exception as ex:
+        print(ex)
+        abort(422)
 
-# @app.route('/movies/<int:movie_id>', methods=['DELETE'])
+
+@bp.route('/movies/<int:movie_id>', methods=['DELETE'])
 # @requires_auth('delete:movie')
-# def delete_movie(token, movie_id):
-#     try:
-#         # Later, add feature to warn user about deleting permanently
-#         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
-#         movie.actors = []
-#         db.session.commit()
+def delete_movie(movie_id):
+    try:
+        # TODO: Add feature to warn user about deleting permanently
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
 
-#         if movie is None:
-#             # print('error')
-#             abort(404)
+        if movie is None:
+            # print('error')
+            abort(404)
 
-#         movie.delete()
+        movie.delete()
 
-#         return jsonify({
-#             'success': True,
-#             'deleted': movie_id,
-#             'total_movies': len(Movie.query.all())
-#         })
+        return jsonify({
+            'success': True,
+            'deleted': movie_id,
+            'total_movies': len(Movie.query.all())
+        })
 
-#     except Exception as ex:
-#         # print(ex)
-#         abort(422)
+    except Exception as ex:
+        # print(ex)
+        abort(422)
 
-# @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+
+@bp.route('/movies/<int:movie_id>', methods=['PATCH'])
 # @requires_auth('patch:movie')
-# def edit_movie(token, movie_id):
-#     body = request.get_json()
-#     new_actors = body.get('actors')
-#     try:
-#         movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
-#         if movie is None:
-#             abort(404)
+def edit_movie(movie_id):
+    body = request.get_json()
+    new_actors = body.get('actors')
+    try:
+        movie = Movie.query.filter(Movie.id == movie_id).one_or_none()
+        if movie is None:
+            abort(404)
 
-#         if 'name' in body:
-#             movie.name = body.get('name')
-#         if 'actors' in body:
-#             current_actors = []
-#         for instance in new_actors:
-#             a = Actor.query.filter(Actor.name == instance).first()
-#             current_actors.append(a)
+        if 'name' in body:
+            movie.name = body.get('name')
+        if 'actors' in body:
+            current_actors = []
+        for instance in new_actors:
+            a = Actor.query.filter(Actor.name == instance).first()
+            current_actors.append(a)
 
-#         movie.actors = current_actors
+        movie.actors = current_actors
 
-#         if 'release_date' in body:
-#             movie.release_date = int(body.get('release_date'))
-#         movie.update()
+        if 'release_date' in body:
+            movie.release_date = int(body.get('release_date'))
+        movie.update()
 
-#         return jsonify({
-#             'success': True,
-#             'edited': movie_id,
-#                         'total_movies': len(Movie.query.all())
-#         })
+        return jsonify({
+            'success': True,
+            'edited': movie_id,
+            'total_movies': len(Movie.query.all())
+        })
 
-#     except Exception as ex:
-#         print(ex)
-#         abort(422)
+    except Exception as ex:
+        print(ex)
+        abort(422)
